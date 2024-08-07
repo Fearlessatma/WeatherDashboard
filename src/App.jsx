f27a2c20644ec69836dbbab5f21545ffd13a7e25
Fearlessatma <94792144+Fearlessatma@users.noreply.github.com>
2024-08-07 18:28:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
   
   return (
     
-    <div className='container w-50 p-5'>
+    <div className='container w-75 p-5'>
       <h1 className='title'>Weather Dashboard</h1>
     <div className="input-group mb-5">
       
